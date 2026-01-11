6749a16ca4806ba5b10ab1f02a403abdbd96ef02
Yan Ferreira <yanfvc@gmail.com>
2026-01-11 18:08:56 -0300

    Project created

---
+++ src/test/java/com/yan/ferreira/cursospringboot/CursospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yan.ferreira.cursospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
