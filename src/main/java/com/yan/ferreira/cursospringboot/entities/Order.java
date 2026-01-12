90c49b0390c8050b990c2c1584e0faae642a82b1
Yan Ferreira <yanfvc@gmail.com>
2026-01-11 23:45:17 -0300

    parte 2 - OrderStatus enum

--- src/main/java/com/yan/ferreira/cursospringboot/entities/Order.java
+++ src/main/java/com/yan/ferreira/cursospringboot/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+import com.yan.ferreira.cursospringboot.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -26,6 +27,8 @@ public class Order implements Serializable {
 
     @JsonFormat(shape = Shape.STRING, pattern = "dd/MM/yyyy'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+
+    private Integer orderStatus;
     
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -34,9 +37,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -54,6 +58,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();            
+        }
     }
 
     public User getClient() {
