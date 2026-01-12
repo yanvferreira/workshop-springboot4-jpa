e5f73cb6ea0943f130bf25f35c79a34dad4810fe
Yan Ferreira <yanfvc@gmail.com>
2026-01-11 21:48:06 -0300

    Service layer, component registration

--- src/main/java/com/yan/ferreira/cursospringboot/resources/UserResource.java
+++ src/main/java/com/yan/ferreira/cursospringboot/resources/UserResource.java
@@ -1,19 +1,35 @@
 package com.yan.ferreira.cursospringboot.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yan.ferreira.cursospringboot.entities.User;
+import com.yan.ferreira.cursospringboot.services.UserService;
+
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User user = new User(1L, "Maria", "maria@gmail.com", "99999", "12345");
+    public ResponseEntity<List<User>> findAll() {
+        List<User> listUsers = service.findAll();
+        return ResponseEntity.ok().body(listUsers);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = service.findById(id);
         return ResponseEntity.ok().body(user);
     }
+    
 }
---
+++ src/main/java/com/yan/ferreira/cursospringboot/services/UserService.java
@@ -0,0 +1,27 @@
+package com.yan.ferreira.cursospringboot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yan.ferreira.cursospringboot.entities.User;
+import com.yan.ferreira.cursospringboot.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.get();
+    }
+
+}
