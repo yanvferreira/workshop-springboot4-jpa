1daf21d4daae04ae51720128b185c68632d84206
Yan Ferreira <yanfvc@gmail.com>
2026-01-15 21:36:19 -0300

    User insert

--- src/main/java/com/yan/ferreira/cursospringboot/resources/UserResource.java
+++ src/main/java/com/yan/ferreira/cursospringboot/resources/UserResource.java
@@ -1,16 +1,21 @@
 package com.yan.ferreira.cursospringboot.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.yan.ferreira.cursospringboot.entities.User;
 import com.yan.ferreira.cursospringboot.services.UserService;
+
 
 
 @RestController
@@ -31,5 +36,13 @@ public class UserResource {
         User user = service.findById(id);
         return ResponseEntity.ok().body(user);
     }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        User user = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
+    }
+    
     
 }
--- src/main/java/com/yan/ferreira/cursospringboot/services/UserService.java
+++ src/main/java/com/yan/ferreira/cursospringboot/services/UserService.java
@@ -13,15 +13,19 @@ import com.yan.ferreira.cursospringboot.repositories.UserRepository;
 public class UserService {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserRepository repository;
 
     public List<User> findAll() {
-        return userRepository.findAll();
+        return repository.findAll();
     }
 
     public User findById(Long id) {
-        Optional<User> user = userRepository.findById(id);
+        Optional<User> user = repository.findById(id);
         return user.get();
+    }
+
+    public User insert(User obj) {
+        return repository.save(obj);
     }
 
 }
